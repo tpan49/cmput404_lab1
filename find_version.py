203d25afa89a9d60811942d00d41b8de3e7f47ab
Tran <phuonganhnguyenn0011@gmail.com>
2023-01-09 18:53:28 -0700

    modified

--- find_version.py
+++ find_version.py
@@ -2,4 +2,4 @@ import requests
 
 print(requests.__version__)
 
-print(requests.get("https://www.google.com").text)
+print(requests.get("https://raw.githubusercontent.com/tpan49/cmput404_lab1/master/find_version.py").text)
